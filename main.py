d09be679e30d4df28c253a34fccdc1ac338efb44
Shiva <35211454+gl0bsec@users.noreply.github.com>
2024-06-27 14:26:30 +0200

    fixed merge fuck up

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #%% 
 from cloud_functions import*
-#%% 
-# from cloud_functions import*
 from download_big_WIP import download_data
 from data_helpers import*
 import os
@@ -22,14 +20,6 @@ def create_index_name(date,query,prefix):
 input_date = '2/10/2019'
 index_name = create_index_name(input_date,country,'test2')
 
-def create_index_name(date,query,prefix):
-    date = date.replace("/", ".")
-    query = query.replace("#",'.').lower()
-    return f"{query}{date}_{prefix}"
-
-
-index_name = create_index_name(input_date,country,'test1')
-
 pwd = None
 directory_path = 'big_dump/'
 
@@ -43,21 +33,5 @@ download_es_index_to_csv(9200, pwd, index_name, f'{index_name}.csv')
 #%% 
 bucket_name = 'hr_news_1'
 upload_to_gcs(bucket_name, f'{index_name}.csv', f'news_test/{index_name}.csv')
-
-# %%
-directory_path = 'big_dump/'
-
-files = os.listdir(directory_path)
-for file in files:
-    create_and_load_es_index(9200, directory_path+file, index_name,pwd)
-
-#%%
-# bucket_name = 'hr_news_1'
-# directory_path = 'big_dump/'
-# files = os.listdir(directory_path)
-
-# for file in files:
-#     destination_blob_name = 'news-test/'+file
-#     upload_to_gcs(bucket_name, directory_path+file, destination_blob_name)
 
 # %%
