cd5c89504fd1ff5291f714ca0a5657849bac9dec
Shiva <35211454+gl0bsec@users.noreply.github.com>
2024-06-27 14:23:24 +0200

    bleep

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#%% 
 from cloud_functions import*
 from download_big_WIP import download_data
 from data_helpers import*
@@ -8,20 +9,29 @@ input_date = '2/10/2019'
 country = '#AO#'
 n = int(int(input('How many days? '))/2)
 download_data(None,n,input_date,locations_regex=country)
-
 # filename = str(input('what is the file name?'))
 # file_input_path = filename+".csv"
 
+def create_index_name(date,query,prefix):
+    date = date.replace("/", ".")
+    query = query.replace("#",'.').lower()
+    return f"{query}{date}_{prefix}"
+
+input_date = '2/10/2019'
+index_name = create_index_name(input_date,country,'test2')
+
 pwd = None
-prefix = 'test'
-index_name = f"{country}{input_date}_{prefix}"
+directory_path = 'big_dump/'
 
-create_and_load_es_index(9200, 'test.csv', 'index_name',pwd)
- 
-# bucket_name = 'hr_news_1'
-# directory_path = 'big_dump/'
-# files = os.listdir(directory_path)
+files = os.listdir(directory_path)
+for file in files:
+    create_and_load_es_index(9200, directory_path+file, index_name,pwd)
 
-# for file in files:
-#     destination_blob_name = 'news-test/'+file
-#     upload_to_gcs(bucket_name, directory_path+file, destination_blob_name)
+os.remove('/big_dump')
+download_es_index_to_csv(9200, pwd, index_name, f'{index_name}.csv')
+
+#%% 
+bucket_name = 'hr_news_1'
+upload_to_gcs(bucket_name, f'{index_name}.csv', f'news_test/{index_name}.csv')
+
+# %%
